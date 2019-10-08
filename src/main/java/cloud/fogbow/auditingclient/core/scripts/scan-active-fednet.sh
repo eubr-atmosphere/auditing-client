5f3b3b3cab55cd33c759340218430fd3c3324cbf
raonismaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-10-08 08:20:51 -0300

    fixing the scripts

--- src/main/java/cloud/fogbow/auditingclient/core/scripts/scan-active-fednet.sh
+++ src/main/java/cloud/fogbow/auditingclient/core/scripts/scan-active-fednet.sh
@@ -10,4 +10,4 @@ psql -U fogbow -d fns -t -c "select json_agg(json_build_object(
   'ip', ip,
   'computeId', compute_id,
   'id', id)) from federated_network_table inner join
-  federated_network_order_assigned_ips on federated_network_order_assigned_ips.federated_network_order_id = federated_network_table.id AND provider = 'UPV'"
+  federated_network_order_assigned_ips on federated_network_order_assigned_ips.federated_network_order_id = federated_network_table.id"
